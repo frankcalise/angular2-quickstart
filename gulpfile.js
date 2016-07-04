4025375177da25cbe8f72b6d242d621d0a15acc1
Frank Calise <fcalise@gmail.com>
2016-07-04 07:53:41 -0400

    Gulp file to output to dist/

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,42 @@
 var gulp      	= require('gulp'),
-    ts          = require('gulp-typescript'),
     sass    	= require('gulp-sass'),
-    minifyCss   = require('gulp-minify-css'),
-    del			= require('del');
-    
-var paths = {
-    appJavascript: ['**/*.ts', '!node_modules/**/*.*'],
+    minifyCss   = require('gulp-minify-css');    
+
+var PATHS = {
+    src: ['**/*.ts', '!node_modules/**/*.*'],
     appScss: ['**/*.scss', '!node_modules/**/*.*']
 }
 
-// load configuration
-var tsProject = ts.createProject('tsconfig.json'); 
+gulp.task('ts', function () {
+	var typescript = require('gulp-typescript');
+	var tscConfig = require('./tsconfig.json');
+	
+    var tsResult = gulp
+        .src(PATHS.src)
+        .pipe(typescript(tscConfig.compilerOptions));
 
-gulp.task('ts', function() {
-	var tsResult = tsProject.src(paths.appJavascript) 	// load all files from our pathspecification
-		.pipe(ts(tsProject)); 							// transpile the files into .js
-    
-	return tsResult.js.pipe(gulp.dest(function(file) {
-		return file.base;
-	})); 			// save the .js in the same place as the original .ts-file
+    return tsResult.js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('sass', function() {
-  gulp.src(paths.appScss)
+gulp.task('sass', function () {
+  gulp.src(PATHS.appScss)
     .pipe(sass().on('error', sass.logError)) 			// this will prevent our future watch-task from crashing on sass-errors
     .pipe(minifyCss({compatibility: 'ie8'})) 			// see the gulp-sass doc for more information on compatibilitymodes
         .pipe(gulp.dest(function(file) {
-            return file.base; 							// because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
+            return 'dist/' + file.base; 							// because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
     }));
 });
 
 gulp.task('watch', ['sass', 'ts'], function() { 		// brackets makes sure we run ts and sass once before the watch starts
-    gulp.watch(paths.appScss, ['sass']); 				// run the sass-task any time stuff in the appScss changes
-    gulp.watch(paths.appJavascript, ['ts']); 			// run the ts-task any time stuff in appJavascript changes
+    gulp.watch(PATHS.appScss, ['sass']); 				// run the sass-task any time stuff in the appScss changes
+    gulp.watch(PATHS.src, ['ts']); 			// run the ts-task any time stuff in src changes
 });
 
 // clean the contents of the distribution directory
-gulp.task('clean', function () {
-  return del('dist/**/*');
+gulp.task('clean', function (done) {
+	var del = require('del');
+  	del(['dist'], done);
 });
+
+// set default task
+gulp.task('default', ['watch']);
